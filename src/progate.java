fc6316c2702d8cc68811b2543eeb445e85a6797b
AKIHIRO WATANABE <answer.is.near.2@gmail.com>
2021-04-05 21:11:11 +0900

    progate-practice

---
+++ src/progate.java
@@ -0,0 +1,11 @@
+public class progate {
+    public static void main(String[] args) {
+//        publicはどこからでも参照可能。
+//        staticはインスタンス可しなくても使用可能
+//        voidは戻り値なし
+//        mainはメソッド名
+//        String[]は引数がString型の配列を受け取るということ！
+//        argsはargment(引数)の略で、かつ複数形！
+        System.out.println("progate practice");
+    }
+}
