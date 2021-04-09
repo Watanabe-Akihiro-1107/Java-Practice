58f3525c48b17700f79f5639521053a85e725d9e
AKIHIRO WATANABE <answer.is.near.2@gmail.com>
2021-04-09 23:48:48 +0900

    eclipse-test

---
+++ sample/src/sample/Sample2_1.java
@@ -0,0 +1,9 @@
+package sample;
+
+public class Sample2_1 {
+	public static void main(String[] args){
+		System.out.println("文字列を"+"連結します");
+		System.out.println(3 * 3);
+		System.out.println("1+1=" + (1+1));
+	}
+}
