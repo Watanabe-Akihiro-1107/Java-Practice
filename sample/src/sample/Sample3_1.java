fb7b170a0357298dafdcbc262f02ca23f39f5825
AKIHIRO WATANABE <answer.is.near.2@gmail.com>
2021-04-10 21:41:49 +0900

    Sample3_1

---
+++ sample/src/sample/Sample3_1.java
@@ -0,0 +1,23 @@
+package sample;
+
+public class Sample3_1 {
+	public static void main(String[] args){
+		boolean a = true;
+		char b = 'A'; //charはシングルクォーテーションで囲まれた一文字
+		byte c = 0; //byteは整数形
+		short d = 32767; //shortは整数形
+		int e = 2147483467; //intも整数形
+		long f = 10000000000L; //longも整数形
+		float g = 15; //floatは単精度浮動少数点形
+		double h = 33.33; //倍精度浮動少数点形
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+	}
+}
