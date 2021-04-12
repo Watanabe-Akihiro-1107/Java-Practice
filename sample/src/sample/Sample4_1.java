fb90b9d62b2b2ceb207f065e0fe328388dbcce23
AKIHIRO WATANABE <answer.is.near.2@gmail.com>
2021-04-12 22:06:15 +0900

    Sample4_1

--- sample/src/sample/Sample4_1.java
+++ sample/src/sample/Sample4_1.java
@@ -1,5 +1,17 @@
 package sample;
 
 public class Sample4_1 {
-
+	public static void main(String[] args){
+		int percent = 3;
+		
+		if(percent>= 50 ){
+			System.out.println("傘持っていきましょう");
+		}
+		else if(percent >= 30 ){
+			System.out.println("折り畳み傘を持っていきましょう");
+		}
+		else{
+			System.out.println("傘は必要ありません。");
+		}
+	}
 }
